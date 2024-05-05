0632802aa9ab3db42eebd18a38b4f3f67323e18e
DannyPhan7om <15460@coderacademy.edu.au>
2024-05-05 13:16:53 +1000

    Added Menu Options

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# Class for the Virtual Pet
 class VirtualPet:
     def __init__(self, name):
         self.name = name
@@ -19,3 +21,22 @@ class VirtualPet:
         self._fullness = 50
         self._happiness = 50
         self._energy = 50
+
+    def status(self):
+        return f"Name: {self.name}, Hunger: {self._fullness}, Happiness: {self._happiness}, Energy: {self._energy}"
+
+
+# Menu Screen
+def create_menu(pet):
+    
+    print(pet.status())
+    
+    print("1. Press 1 to feed your pet")
+    print("2. Press 2 to let your pet rest")
+    print("3. Press 3 to play with your pet")
+    print("4. Press 4 to EXIT")
+
+    user_choice = input("Pick your action:  ")
+    print(user_choice)
+    return user_choice
+
